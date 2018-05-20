5fe028b3c863c3c431e9951a64391f0061566e88
AminuSufi585 <aminukano585@gmail.com>
2018-05-21 00:55:00 +0100

    pass the GET all requests route test [fixes #157723855]

--- test/index.js
+++ test/index.js
@@ -15,3 +15,14 @@ describe('Test Express Server Setup', () => {
             });
     });
 });
+
+describe('/GET requests', () => {
+    it('should GET all the requests', done => {
+        chai.request(server)
+            .get('/api/v1/users/requests')
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
